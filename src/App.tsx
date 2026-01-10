255b981cfe64c71c0d845b180c997d208cd3d7c9
Pratik <pratiky2000@gmail.com>
2026-01-11 00:56:58 +0530

    Refactor App component layout and integrate Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+// import Layout from "@Layout/components/layout/Layout"
+
+import Navbar from "./components/shared/Navbar/Navbar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="w-full h-full flex flex-col items-start justify-center p-6 pl-1">
+      <Navbar/>
+      <div className="bg-secondary w-full h-full rounded-2xl border-border-1 border">
+        dfsdf
+      </div>   
+    </div>
   )
 }
 
