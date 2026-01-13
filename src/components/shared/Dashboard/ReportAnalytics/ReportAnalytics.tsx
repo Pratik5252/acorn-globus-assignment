7341f94a6deb5f1a4262269076f091d6bc03bedb
Pratik <pratiky2000@gmail.com>
2026-01-13 11:04:47 +0530

    fix: adjust layout

--- src/components/shared/Dashboard/ReportAnalytics/ReportAnalytics.tsx
+++ src/components/shared/Dashboard/ReportAnalytics/ReportAnalytics.tsx
@@ -6,7 +6,7 @@ import UserStatsTable from '../UserStatsTable/UserStatsTable'
 const ReportAnalytics = () => {
   return (
     <div className='flex gap-6 mt-4 pt-2 pb-0'>
-        <div className='flex flex-col w-1/2 gap-2'>
+        <div className='flex flex-col w-1/2 h-full gap-2'>
             <div className='flex gap-2'>
                 <RevenueByPlatform/>
                 <DealsAmountChart/>
