39b3f31bf5a25febd333781d346a0a5fdaa3af7f
Pratik <pratiky2000@gmail.com>
2026-01-13 17:15:29 +0530

    fix: improve Navbar layout and integrate SidebarTrigger for better responsiveness

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,22 +1,32 @@
-import { Menu, Plus} from "lucide-react"
-import SearchBar from "./SearchBar"
-import { Button } from "@/components/ui/button"
+import { Menu, Plus } from "lucide-react";
+import SearchBar from "./SearchBar";
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex items-center justify-between mb-4">
-        <SearchBar/>
-        <div className="flex items-center gap-4">
-            <div className="bg-secondary h-9 flex items-center gap-2 rounded-full px-1 pl-2 py-1 border border-border-1">
-                <Menu size={16} className=""/>
-                <img src="https://picsum.photos/seed/picsum/200/300" alt="random_image" className="w-7 h-7 rounded-full"/>
-            </div>
-            <Button className="bg-accent w-9 h-9 rounded-full flex items-center justify-center cursor-pointer hover:bg-accent/80 active:bg-accent/90 border border-border-1">
-                <Plus size={16} className="text-white"/>
-            </Button>
+    <div className="w-full flex items-center justify-between mb-2 md:mb-4">
+      <div className="flex items-center justify-center gap-4 ml-2 lg:ml-0">
+        <div className="flex items-center lg:hidden">
+          <SidebarTrigger className="cursor-pointer"/>
         </div>
+        <SearchBar />
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="bg-secondary h-9 flex items-center gap-2 rounded-full px-1 pl-2 py-1 border border-border-1">
+          <Menu size={16} className="" />
+          <img
+            src="https://picsum.photos/seed/picsum/200/300"
+            alt="random_image"
+            className="w-7 h-7 rounded-full"
+          />
+        </div>
+        <Button className="bg-accent w-9 h-9 rounded-full flex items-center justify-center cursor-pointer hover:bg-accent/80 active:bg-accent/90 border border-border-1">
+          <Plus size={16} className="text-white" />
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
